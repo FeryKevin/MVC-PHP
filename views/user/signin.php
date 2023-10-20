51812c72aa52f328fea1d079cb579a373ebd9890
FeryKevin <kevin.fery@outlook.com>
2023-10-20 15:11:58 +0200

    add signin front

--- views/user/signin.php
+++ views/user/signin.php
@@ -1,13 +1,18 @@
-<h1>Inscription</h1>
+<h2>Inscription</h2>
+<?= isset($message) ? "<p class='error'>".$message."</p>" : "" ?>
 <form action="/signin" method="post">
-    <label for="username">Nom :</label>
-    <input type="text" id="username" name="username" required>
-
-    <label for="email">Email :</label>
-    <input type="text" id="email" name="email" required>
-
-    <label for="password">Mot de passe :</label>
-    <input type="text" id="password" name="password" required>
-
-    <button type="submit">Ibscr</button>
+    <div class="form-crud">
+        <label for="username">Nom :</label>
+        <input type="text" id="username" name="username" required>
+        <label for="email">Email :</label>
+        <input type="text" id="email" name="email" required>
+        <label for="password">Mot de passe :</label>
+        <input type="text" id="password" name="password" required>
+    </div>    
+    <div class="btn">
+        <button type="submit" class="btn-add">Inscription</button>
+    </div>
+    <div class="btn2">
+        <a href='/login' class="btn-retour">Connexion</a>
+    </div>
 </form>
